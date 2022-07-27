df288416e23dd5efc8fc7438fab140717c622094
Daria Sukhonina <dariasukhonina@gmail.com>
2022-07-27 14:12:39 +0300

    psvita: add sys::Mutex

--- library/std/src/sys/psvita/locks/mutex.rs
+++ library/std/src/sys/psvita/locks/mutex.rs
@@ -1,36 +1,88 @@
-use crate::cell::Cell;
-
-pub struct Mutex {
-    // This platform has no threads, so we can use a Cell here.
-    locked: Cell<bool>,
-}
+use crate::ptr;
+use crate::sync::atomic::{self, AtomicPtr};
 
 pub type MovableMutex = Mutex;
 
-unsafe impl Send for Mutex {}
-unsafe impl Sync for Mutex {} // no threads on this platform
+pub struct Mutex {
+    inner: AtomicPtr<libc::mtx_impl_t>,
+}
+
+#[inline]
+pub fn raw(mutex: &Mutex) -> libc::mtx_t {
+    mutex.inner.load(atomic::Ordering::Acquire)
+}
 
 impl Mutex {
     #[inline]
     pub const fn new() -> Mutex {
-        Mutex { locked: Cell::new(false) }
+        Mutex { inner: AtomicPtr::new(ptr::null_mut()) }
     }
 
     #[inline]
-    pub unsafe fn init(&mut self) {}
+    pub unsafe fn init(&mut self) {
+        *self.inner.get_mut() = Self::create_inner();
+    }
+
+    #[inline]
+    fn init_once(&self) -> libc::mtx_t {
+        let mutex = self.inner.load(atomic::Ordering::Acquire);
+        if mutex.is_null() { self.start_init() } else { mutex }
+    }
+
+    #[cold]
+    fn start_init(&self) -> libc::mtx_t {
+        let mut mutex = Self::create_inner();
+        match self.inner.compare_exchange(
+            ptr::null_mut(),
+            mutex,
+            atomic::Ordering::AcqRel,
+            atomic::Ordering::Acquire,
+        ) {
+            Ok(_) => mutex,
+            Err(older_mutex) => {
+                unsafe { libc::mtx_destroy(&mut mutex) }
+                older_mutex
+            }
+        }
+    }
+
+    #[inline]
+    fn create_inner() -> libc::mtx_t {
+        let mut mutex = ptr::null_mut();
+        assert_eq!(libc::thrd_success, unsafe { libc::mtx_init(&mut mutex, libc::mtx_plain) });
+        mutex
+    }
 
     #[inline]
     pub unsafe fn lock(&self) {
-        assert_eq!(self.locked.replace(true), false, "cannot recursively acquire mutex");
+        let mut mutex = self.init_once();
+        assert_eq!(
+            libc::thrd_success,
+            unsafe { libc::mtx_lock(&mut mutex) },
+            "Perhaps current thread already owns the lock to the mutex"
+        );
     }
 
     #[inline]
     pub unsafe fn unlock(&self) {
-        self.locked.set(false);
+        let mut mutex = self.init_once();
+        assert_eq!(libc::thrd_success, unsafe { libc::mtx_unlock(&mut mutex) });
     }
 
     #[inline]
     pub unsafe fn try_lock(&self) -> bool {
-        self.locked.replace(true) == false
+        let mut mutex = self.init_once();
+        match unsafe { libc::mtx_trylock(&mut mutex) } {
+            libc::thrd_success => true,
+            libc::thrd_busy => false,
+            other => panic!("error code: {}", other),
+        }
+    }
+}
+
+impl Drop for Mutex {
+    fn drop(&mut self) {
+        // Performs the null check internally
+        unsafe { libc::mtx_destroy(self.inner.get_mut()) }
     }
 }
