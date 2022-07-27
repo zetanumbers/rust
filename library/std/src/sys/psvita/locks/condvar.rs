10f52251b25fc467052d529d56b6393c2f5a0228
Daria Sukhonina <dariasukhonina@gmail.com>
2022-07-27 16:44:22 +0300

    psvita: refactor `sys::Condvar`

--- library/std/src/sys/psvita/locks/condvar.rs
+++ library/std/src/sys/psvita/locks/condvar.rs
@@ -1,4 +1,4 @@
-use crate::ptr;
+use crate::ptr::{self, NonNull};
 use crate::sync::atomic::{self, AtomicPtr};
 use crate::sys::locks::{mutex, Mutex};
 use crate::time::Duration;
@@ -16,44 +16,56 @@ impl Condvar {
     }
 
     #[inline]
-    fn init_once(&self) -> libc::cnd_t {
+    fn try_get(&self) -> Option<NonNull<libc::cnd_impl_t>> {
         let cond = self.inner.load(atomic::Ordering::Acquire);
-        if cond.is_null() { self.start_init() } else { cond }
+        NonNull::new(cond)
+    }
+
+    #[inline]
+    fn init_once(&self) -> NonNull<libc::cnd_impl_t> {
+        self.try_get().unwrap_or_else(|| self.start_init())
     }
 
     #[cold]
-    fn start_init(&self) -> libc::cnd_t {
-        let mut cond = ptr::null_mut();
-        assert_eq!(libc::thrd_success, unsafe { libc::cnd_init(&mut cond) });
+    fn start_init(&self) -> NonNull<libc::cnd_impl_t> {
+        let cond = Self::create_inner();
         match self.inner.compare_exchange(
             ptr::null_mut(),
-            cond,
+            cond.as_ptr(),
             atomic::Ordering::AcqRel,
             atomic::Ordering::Acquire,
         ) {
             Ok(_) => cond,
             Err(older_mutex) => {
+                let mut cond = cond.as_ptr();
                 unsafe { libc::cnd_destroy(&mut cond) }
-                older_mutex
+                NonNull::new(older_mutex).unwrap()
             }
         }
     }
 
     #[inline]
+    fn create_inner() -> NonNull<libc::cnd_impl_t> {
+        let mut cond = ptr::null_mut();
+        assert_eq!(libc::thrd_success, unsafe { libc::cnd_init(&mut cond) });
+        NonNull::new(cond).unwrap()
+    }
+
+    #[inline]
     pub unsafe fn notify_one(&self) {
-        let mut cond = self.init_once();
+        let mut cond = self.init_once().as_ptr();
         assert_eq!(libc::thrd_success, unsafe { libc::cnd_signal(&mut cond) })
     }
 
     #[inline]
     pub unsafe fn notify_all(&self) {
-        let mut cond = self.init_once();
+        let mut cond = self.init_once().as_ptr();
         assert_eq!(libc::thrd_success, unsafe { libc::cnd_broadcast(&mut cond) })
     }
 
     #[inline]
     pub unsafe fn wait(&self, mutex: &Mutex) {
-        let mut cond = self.init_once();
+        let mut cond = self.init_once().as_ptr();
 
         let mut mutex = mutex::raw(mutex);
         // mutex is locked, it cannot be null
@@ -64,7 +76,6 @@ impl Condvar {
         let mut xtime = libc::xtime { sec: 0, nsec: 0 };
         assert_eq!(libc::TIME_UTC, unsafe { libc::xtime_get(&mut xtime, libc::TIME_UTC) });
 
-        // use wrapping arithmetic since implementation computes duration underneath
         let dur_nanos_before_next_sec = 1_000_000_000 - (dur.subsec_nanos() as i32);
         if dur_nanos_before_next_sec <= xtime.nsec {
             xtime.sec = xtime.sec.saturating_add(1);
@@ -74,7 +85,7 @@ impl Condvar {
         }
         xtime.sec = xtime.sec.saturating_add(dur.as_secs().min(i32::MAX as u64) as i32);
 
-        let mut cond = self.init_once();
+        let mut cond = self.init_once().as_ptr();
         let mut mutex = mutex::raw(mutex);
 
         // mutex is locked, it cannot be null
