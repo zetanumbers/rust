035bf6e39cd24b9232338ebd07fbf068fd809b7c
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-03-26 17:55:32 +0100

    Update exclusion list in build_llvm_sysroot_for_triple

--- build_system/build_sysroot.rs
+++ build_system/build_sysroot.rs
@@ -178,9 +178,7 @@ fn build_llvm_sysroot_for_triple(compiler: Compiler) -> SysrootTarget {
             && !file_name_str.contains("rustc_std_workspace_")
             && !file_name_str.contains("rustc_demangle")
             && !file_name_str.contains("rustc_literal_escaper"))
-            || file_name_str.contains("chalk")
-            || file_name_str.contains("tracing")
-            || file_name_str.contains("regex")
+            || file_name_str.contains("LLVM")
         {
             // These are large crates that are part of the rustc-dev component and are not
             // necessary to run regular programs.
