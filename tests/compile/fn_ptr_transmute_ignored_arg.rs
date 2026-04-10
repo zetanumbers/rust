c8634656fe12f1a1dada0b095364349b1fa6c1a1
cijiugechu <cijiugechu@gmail.com>
2026-04-10 15:12:13 +0800

    Add `#[unsafe(no_mangle)]`

--- tests/compile/fn_ptr_transmute_ignored_arg.rs
+++ tests/compile/fn_ptr_transmute_ignored_arg.rs
@@ -4,6 +4,7 @@
 
 #![crate_type = "lib"]
 
+#[unsafe(no_mangle)]
 extern "C" fn third(_a: usize, b: usize, c: usize) {
     let throw_away_f: fn((), usize, usize) =
         unsafe { std::mem::transmute(third as extern "C" fn(_, _, _)) };
