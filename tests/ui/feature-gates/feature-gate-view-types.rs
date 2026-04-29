77fb8d1d5c5f5e2058049b3b90abde3f8bccef4f
Sasha Pourcelot <sasha.pourcelot@protonmail.com>
2026-04-29 09:03:26 +0000

    Parse view type syntax, mark it as unstable

--- tests/ui/feature-gates/feature-gate-view-types.rs
+++ tests/ui/feature-gates/feature-gate-view-types.rs
@@ -1,11 +1,11 @@
-//@ known-bug: #155938
-
 struct Foo {
     a: usize,
     b: usize,
 }
 
 fn bar(a: &mut Foo.{ a }, b: &mut Foo.{ b }) {
+    //~^ ERROR view types are experimental
+    //~| ERROR view types are experimental
     a.a += 1;
     b.b += 1;
 }
@@ -13,4 +13,5 @@ fn bar(a: &mut Foo.{ a }, b: &mut Foo.{ b }) {
 fn main() {
     let mut foo = Foo { a: 0, b: 0 };
     bar(&mut foo, &mut foo);
+    //~^ ERROR cannot borrow `foo` as mutable more than once at a time
 }
