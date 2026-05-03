9c31fb53a2ef7e5e8deb11b8c9c1059ffc6f7b98
A4-Tacks <wdsjxhno1001@163.com>
2026-05-04 03:52:43 +0800

    fix: don't panic on `impl ?Sized` for introduce_named_type_parameter
    Example
    ---
    ```rust
    fn foo(bar: &$0impl ?Sized) {}
    ```
    
    **Before this PR**
    
    ```
    panic: Failed to make ast node `syntax::ast::generated::nodes::Name` from text mod ?;
    ```
    
    **After this PR**
    
    ```rust
    fn foo<$0S: ?Sized>(bar: &S) {}
    ```

--- src/tools/rust-analyzer/crates/ide-assists/src/handlers/introduce_named_type_parameter.rs
+++ src/tools/rust-analyzer/crates/ide-assists/src/handlers/introduce_named_type_parameter.rs
@@ -183,6 +183,15 @@ fn foo<
     }
 
     #[test]
+    fn replace_impl_question_bounds() {
+        check_assist(
+            introduce_named_type_parameter,
+            r#"fn foo(bar: &$0impl ?Sized) {}"#,
+            r#"fn foo<$0S: ?Sized>(bar: &S) {}"#,
+        );
+    }
+
+    #[test]
     fn replace_impl_with_mut() {
         check_assist(
             introduce_named_type_parameter,
