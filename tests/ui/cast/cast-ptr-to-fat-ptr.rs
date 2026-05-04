32a3612a2bb3964fe5376c498d3e9c04eadd55eb
danieljofficial <joenah004@gmail.com>
2026-05-04 16:02:23 +0100

    add issue links and bless

--- tests/ui/cast/cast-ptr-to-fat-ptr.rs
+++ tests/ui/cast/cast-ptr-to-fat-ptr.rs
@@ -1,3 +1,5 @@
+//! Regression test for https://github.com/rust-lang/rust/issues/30615
+
 //@ run-pass
 fn main() {
     &0u8 as *const u8 as *const dyn PartialEq<u8>;
--- tests/ui/cast/invalid-thin-to-fat-pointer-cast.rs
+++ tests/ui/cast/invalid-thin-to-fat-pointer-cast.rs
@@ -1,3 +1,5 @@
+//! Regression test for https://github.com/rust-lang/rust/issues/31511
+
 fn cast_thin_to_fat(x: *const ()) {
     x as *const [u8];
     //~^ ERROR: cannot cast thin pointer `*const ()` to wide pointer `*const [u8]`
