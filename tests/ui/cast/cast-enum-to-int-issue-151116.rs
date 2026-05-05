0f5d89fea1d8751a94e2e023959743da7516dc80
Jaiden Magnan <jaidenmagnan@gmail.com>
2026-05-05 17:34:50 -0400

    fix: CopyPriority addition

--- tests/ui/cast/cast-enum-to-int-issue-151116.rs
+++ tests/ui/cast/cast-enum-to-int-issue-151116.rs
@@ -5,6 +5,14 @@ enum Priority { //~ HELP: add `#[derive(Copy, Clone)]` to the enum definition
     Low = 1,
 }
 
+#[repr(u8)]
+#[derive(Copy, Clone)]
+enum CopyPriority {
+    High = 255,
+    Normal = 127,
+    Low = 1,
+}
+
 fn main() {
     let priority = &Priority::Normal;
     let priority = priority as u8; //~ ERROR casting `&Priority` as `u8` is invalid
@@ -12,4 +20,8 @@ fn main() {
 
     let priority = &Priority::Normal as u8; //~ ERROR casting `&Priority` as `u8` is invalid
     //~| HELP: cast through a raw pointer first
+
+    let priority = &CopyPriority::Normal;
+    let priority = priority as u8; //~ ERROR casting `&CopyPriority` as `u8` is invalid
+    //~| HELP: try dereferencing before the cast
 }
