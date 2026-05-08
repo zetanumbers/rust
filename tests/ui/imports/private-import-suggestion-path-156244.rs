6d5fc3275f99116946190ef1d913312f3f24af9d
Iris Shi <0.0@owo.li>
2026-05-08 20:42:22 +0800

    combine tests into one file

--- tests/ui/imports/private-import-suggestion-path-156244.rs
+++ tests/ui/imports/private-import-suggestion-path-156244.rs
@@ -1,16 +1,42 @@
 // PR #156244 comment
+//@ revisions: edition_2015 edition_2018
+//@[edition_2015] edition: 2015
+//@[edition_2018] edition: 2018
 
-mod one {
-    pub struct One();
+mod a {
+    pub struct One;
+    pub struct Two;
 }
 
-mod two {
-    use crate::one::One;
+mod b {
+    use crate::a::{One, Two};
 }
 
 mod test {
-    use crate::two::One;
+    #[cfg(edition_2015)]
+    use b::One;
+    //[edition_2015]~^ ERROR struct import `One` is private [E0603]
+    #[cfg(edition_2018)]
+    use crate::b::One;
+    //[edition_2018]~^ ERROR struct import `One` is private [E0603]
+}
+
+mod outer {
+    pub mod actual {
+        pub struct Item;
+    }
+}
+
+mod rename {
+    use crate::outer::actual as inner;
+}
+
+mod bad {
+    use crate::b::{One, Two};
     //~^ ERROR struct import `One` is private [E0603]
+    //~| ERROR struct import `Two` is private [E0603]
+    use crate::rename::inner::Item as Item1;
+    //~^ ERROR module import `inner` is private [E0603]
 }
 
 fn main() {}
